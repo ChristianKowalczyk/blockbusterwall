cbbd446001f64692d81a7f08d566f5eacc87814b
Christian Kowalczyk <chriskowalczyk@Mac.lan>
2025-07-07 19:47:49 -0400

    Movie cleanup

--- src/data/movies.js
+++ src/data/movies.js
@@ -1,22 +1,24 @@
 import CarriePoster from '/pics/carrie.jpg';
 import SuspiriaPoster from '/pics/suspiria.jpg';
 import ShiningPoster from '/pics/theshining.jpg';
+import ChuckyPoster from '/pics/chucky.png'
 
 const movies = [
   {
     title: 'Carrie',
     image: CarriePoster,
-    description: 'Carrie movie desc.'
   },
   {
     title: 'Suspiria',
     image: SuspiriaPoster,
-    description: 'Dancers scary.'
   },
   {
     title: 'The Shining',
     image: ShiningPoster,
-    description: 'Heres johnny.'
+  },
+  {
+    title: 'Chucky',
+    image: ChuckyPoster,
   },
 ];
 
